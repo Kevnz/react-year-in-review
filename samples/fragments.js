857da5191840a34e730e9c44633c94456692eea1
Kevin Isom <kevin.isom@gmail.com>
2018-11-12 08:54:26 +1300

    fragments samples

---
+++ samples/fragments.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from 'react'
+
+export default class fragments extends Component {
+  render() {
+    return (
+      <Fragment>
+        <div>One</div>
+        <div>Two</div>
+      </Fragment>
+    )
+  }
+}
---
+++ samples/fragments2.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class fragments extends Component {
+  render() {
+    return (
+      <>
+        <div>One</div>
+        <div>Two</div>
+      </>
+    )
+  }
+}
