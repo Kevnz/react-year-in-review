1050c2991ef9d9a3383418caf3b1faba6ec789e3
Kevin Isom <kevin.isom@gmail.com>
2018-11-12 08:55:59 +1300

    create ref sample

---
+++ samples/createRef.js
@@ -0,0 +1,14 @@
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.inputRef = React.createRef();
+  }
+
+  render() {
+    return <input type="text" ref={this.inputRef} />;
+  }
+
+  componentDidMount() {
+    this.inputRef.current.focus();
+  }
+}
