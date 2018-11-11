17cb411b5f56a2f6189ae1984dc31109004383e5
Kevin Isom <kevin.isom@gmail.com>
2018-11-12 08:55:27 +1300

    strict mode sample

---
+++ samples/strictmode.js
@@ -0,0 +1,16 @@
+import React, { StrictMode } from 'react';
+
+function ExampleApplication() {
+  return (
+    <div>
+      <Header />
+      <StrictMode>
+        <div>
+          <ComponentOne />
+          <ComponentTwo />
+        </div>
+      </StrictMode>
+      <Footer />
+    </div>
+  );
+}
