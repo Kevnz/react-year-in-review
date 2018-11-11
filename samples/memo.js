5dfcf3157c96fee0c7361846b4eb1c23a49caa7a
Kevin Isom <kevin.isom@gmail.com>
2018-11-12 08:55:14 +1300

    memo

---
+++ samples/memo.js
@@ -0,0 +1,15 @@
+const MyComponent = React.memo(function MyComponent(props) {
+  /* render using props */
+})
+
+function MyComponent(props) {
+  /* render using props */
+}
+function areEqual(prevProps, nextProps) {
+  /*
+  return true if passing nextProps to render would return
+  the same result as passing prevProps to render,
+  otherwise return false
+  */
+}
+export default React.memo(MyComponent, areEqual)
