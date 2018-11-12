d2af3e94468def59548122644bc02ab477e01eff
Kevin Isom <kevin.isom@gmail.com>
2018-11-12 22:24:49 +1300

    useContext sample

---
+++ samples/use-context.js
@@ -0,0 +1,19 @@
+import React, { useContext } from 'react'
+import AppContext from './app-context'
+const Green = () => {
+  const context = useContext(AppContext)
+  return (
+    <div className="green">
+      {context.number}
+    </div>
+  )
+}
+const Blue = () => {
+  const context = useContext(AppContext)
+  return (
+    <div className="blue">
+      <button onClick={context.inc}>INC</button>
+      <Green />
+    </div>
+  )
+}
