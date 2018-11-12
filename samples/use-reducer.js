7dab0cb44f5e8bb09ae4cbe13af220c4b8d15eb3
Kevin Isom <kevin.isom@gmail.com>
2018-11-12 22:25:28 +1300

    useReducer sample

---
+++ samples/use-reducer.js
@@ -0,0 +1,16 @@
+const initialState = {count: 0};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'reset':
+      return initialState;
+    case 'increment':
+      return {count: state.count + 1};
+    case 'decrement':
+      return {count: state.count - 1};
+    default:
+      // A reducer must always return a valid state.
+      // Alternatively you can throw an error if an invalid action is dispatched.
+      return state;
+  }
+}
